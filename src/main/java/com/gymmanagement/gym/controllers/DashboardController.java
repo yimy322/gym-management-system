5e565a55e6681e561e0b9f51caaae8f674b2d7f4
Yimy <fh2001yj@gmail.com>
2026-06-01 18:40:27 -0500

    feat: agregar busqueda de afiliados por dni y nombre - STRK-9 #done

--- src/main/java/com/gymmanagement/gym/controllers/DashboardController.java
+++ src/main/java/com/gymmanagement/gym/controllers/DashboardController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.gymmanagement.gym.services.MemberService;
 import com.gymmanagement.gym.services.PaymentService;
 
 import lombok.RequiredArgsConstructor;
@@ -15,12 +16,14 @@ import lombok.RequiredArgsConstructor;
 public class DashboardController {
 
     private final PaymentService paymentService;
+    private final MemberService memberService;
 
     @GetMapping("/dashboard")
     public String index(Model model) {
         BigDecimal totalIncome = paymentService.getTotalAmount();
         model.addAttribute("activePage", "dashboard");
         model.addAttribute("totalIncome", totalIncome);
+        model.addAttribute("countActiveMembers", memberService.countByStatusTrue());
         return "dashboard";
     }
 
--- src/main/java/com/gymmanagement/gym/repository/MemberRepository.java
+++ src/main/java/com/gymmanagement/gym/repository/MemberRepository.java
@@ -1,5 +1,7 @@
 package com.gymmanagement.gym.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,13 @@ import com.gymmanagement.gym.entities.Member;
 public interface MemberRepository extends JpaRepository<Member, Long>{
 
     boolean existsByDni(String dni);
+
+    Long countByStatusTrue();
+
+    List<Member> findByDniContainingIgnoreCaseOrFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(
+            String dni,
+            String firstName,
+            String lastName
+    );
 
 }
